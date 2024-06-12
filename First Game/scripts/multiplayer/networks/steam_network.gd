7667d38a2450b380c4298c58ac03363c018f8a04
BatteryAcid <battery4cid@gmail.com>
2024-06-12 13:51:09 -0400

    Added support for SteamMultiplayerPeer using GDExtension
    - no longer works with pre-compiled custom Godot build

--- First Game/scripts/multiplayer/networks/steam_network.gd
+++ First Game/scripts/multiplayer/networks/steam_network.gd
@@ -5,30 +5,26 @@ var multiplayer_peer: SteamMultiplayerPeer = SteamMultiplayerPeer.new()
 var _players_spawn_node
 var _hosted_lobby_id = 0
 
-const LOBBY_NAME = "BAD"
+const LOBBY_NAME = "BAD2233"
 const LOBBY_MODE = "CoOP"
 
 func  _ready():
-	multiplayer_peer.lobby_created.connect(_on_lobby_created)
+	#multiplayer_peer.lobby_created.connect(_on_lobby_created)
+	Steam.lobby_created.connect(_on_lobby_created.bind())
 
 func become_host():
 	print("Starting host!")
 	
-	multiplayer_peer.create_lobby(SteamMultiplayerPeer.LOBBY_TYPE_PUBLIC, SteamManager.lobby_max_members)
-	
-	multiplayer.multiplayer_peer = multiplayer_peer
-	
 	multiplayer.peer_connected.connect(_add_player_to_game)
 	multiplayer.peer_disconnected.connect(_del_player)
-
-	if not OS.has_feature("dedicated_server"):
-		_add_player_to_game(1)
+	
+	Steam.lobby_joined.connect(_on_lobby_joined.bind())
+	Steam.createLobby(Steam.LOBBY_TYPE_PUBLIC, SteamManager.lobby_max_members)
 	
 func join_as_client(lobby_id):
 	print("Joining lobby %s" % lobby_id)
-	
-	multiplayer_peer.connect_lobby(lobby_id)
-	multiplayer.multiplayer_peer = multiplayer_peer
+	Steam.lobby_joined.connect(_on_lobby_joined.bind())
+	Steam.joinLobby(int(lobby_id))
 
 func _on_lobby_created(connect: int, lobby_id):
 	print("On lobby created")
@@ -40,12 +36,59 @@ func _on_lobby_created(connect: int, lobby_id):
 		
 		Steam.setLobbyData(_hosted_lobby_id, "name", LOBBY_NAME)
 		Steam.setLobbyData(_hosted_lobby_id, "mode", LOBBY_MODE)
+		
+		_create_host()
+
+func _create_host():
+	print("Create Host")
+	
+	var error = multiplayer_peer.create_host(0, [])
+	
+	if error == OK:
+		multiplayer.set_multiplayer_peer(multiplayer_peer)
+		
+		if not OS.has_feature("dedicated_server"):
+			_add_player_to_game(1)
+	else:
+		print("error creating host: %s" % str(error))
+
+func _on_lobby_joined(lobby: int, permissions: int, locked: bool, response: int):
+	print("On lobby joined")
+	
+	if response == 1:
+		var id = Steam.getLobbyOwner(lobby)
+		if id != Steam.getSteamID():
+			print("Connecting client to socket...")
+			connect_socket(id)
+	else:
+		# Get the failure reason
+		var FAIL_REASON: String
+		match response:
+			2:  FAIL_REASON = "This lobby no longer exists."
+			3:  FAIL_REASON = "You don't have permission to join this lobby."
+			4:  FAIL_REASON = "The lobby is now full."
+			5:  FAIL_REASON = "Uh... something unexpected happened!"
+			6:  FAIL_REASON = "You are banned from this lobby."
+			7:  FAIL_REASON = "You cannot join due to having a limited account."
+			8:  FAIL_REASON = "This lobby is locked or disabled."
+			9:  FAIL_REASON = "This lobby is community locked."
+			10: FAIL_REASON = "A user in the lobby has blocked you from joining."
+			11: FAIL_REASON = "A user you have blocked is in the lobby."
+		print(FAIL_REASON)
+	
+func connect_socket(steam_id: int):
+	var error = multiplayer_peer.create_client(steam_id, 0, [])
+	if error == OK:
+		print("Connecting peer to host...")
+		multiplayer.set_multiplayer_peer(multiplayer_peer)
+	else:
+		print("Error creating client: %s" % str(error))
 
 func list_lobbies():
 	Steam.addRequestLobbyListDistanceFilter(Steam.LOBBY_DISTANCE_FILTER_WORLDWIDE)
 	# NOTE: If you are using the test app id, you will need to apply a filter on your game name
 	# Otherwise, it may not show up in the lobby list of your clients
-	Steam.addRequestLobbyListStringFilter("name", "BAD", Steam.LOBBY_COMPARISON_EQUAL)
+	Steam.addRequestLobbyListStringFilter("name", LOBBY_NAME, Steam.LOBBY_COMPARISON_EQUAL)
 	Steam.requestLobbyList()
 
 func _add_player_to_game(id: int):
